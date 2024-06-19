d0087c561d5c950a17e35d34be3f5f51604c2950
20232543 <shdh004@ess.ais.ac.nz>
2024-06-19 14:59:17 +1200

    Weather App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react";
+
+const api = {
+    key: '3c6562b8d3ba7f6020493029f9228eaf',
+    base: 'https://api.openweathermap.org/data/2.5/'
+}
 
 function App() {
+    const [search,setSearch] = useState('')
+    const [weather,setWeather] = useState('')
+
+    const searchPressed = () => {
+        fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+            .then(res => res.json())
+            .then(result => {
+
+                // setWeather(result)
+                console.log(result)
+                setWeather(result);
+            })
+    }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {/*{Weather}*/}
+          <h1>Weather App</h1>
+          {/*Search Box*/}
+          <div>
+          <input type="text" placeholder="Enter City/Town" onChange={(e) => setSearch(e.target.value)} />
+          <button onClick={searchPressed}>Search</button>
+          </div>
+          {typeof weather.main != "undefined" ? (
+                  <div>
+                      <p>{weather.name}</p>
+                      <p>{weather.main.temp}</p>
+                      <p>{weather.weather[0].main}</p>
+                      <p>{weather.weather[0].description}</p>
+                  </div>
+              ) : (
+                  ''
+          )}
       </header>
     </div>
   );
